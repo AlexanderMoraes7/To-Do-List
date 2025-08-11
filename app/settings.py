d22ea54e970bd2e5f47adfb909033accb93a8305
Alex <alexander97643@gmail.com>
2025-08-11 20:30:20 -0300

    modified:   app/settings.py 	modified:   app/urls.py 	modified:   login/templates/login.html 	modified:   login/views.py 	deleted:    media/todo-list.jpg

--- app/settings.py
+++ app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login',
 ]
 
 MIDDLEWARE = [
@@ -117,7 +118,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'media']
--- login/views.py
+++ login/views.py
@@ -1,1 +1,24 @@
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+
+
+def login_view(request: HttpRequest):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        auth = authenticate(request, username, password)
+        if auth is not None:
+            login(request, auth)
+            return redirect('feed')
+        else:
+            login_form = AuthenticationForm(request, data=request.POST)
+    else:
+        login_form = AuthenticationForm()
+    
+    return render(
+        request,
+        'login.html',
+        {'login_form': login_form}
+    )
